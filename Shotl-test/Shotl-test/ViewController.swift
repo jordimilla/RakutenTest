2f3f0d43ec106a581f751e2b70f2340af330e449
Jordi Milla Catalán <tipshopcompany@gmail.com>
2019-12-04 17:35:23 +0100

    Create project

---
+++ Shotl-test/Shotl-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shotl-test
+//
+//  Created by Jordi Milla on 04/12/2019.
+//  Copyright © 2019 Jordi Milla Catalan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
